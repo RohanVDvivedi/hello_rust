14e3a5cf09c1c0db80ef4664e63e7bf9e79a8310
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 06:38:13 -0700

    added a closure definition to functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -3,6 +3,8 @@ pub fn run () {
     let a : i32 = 32;
     let b : i32 = 64;
     println!("{} + {} = {}", a, b, add(a,b));
+    let cadd = |n1 : i32, n2 : i32| -> i32 { return n1 + n2;};
+    println!("{} + {} = {}", a, b, cadd(a,b));
 }
 
 fn greeting(greet : &str, name : &str) {

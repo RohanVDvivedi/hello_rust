16159183334df623bed6d4862166e871d62db7a2
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 08:32:00 -0700

    learned to define and use a Tuple struct

--- src/structs.rs
+++ src/structs.rs
@@ -1,17 +1,28 @@
 
 // Traditional c style struct
-struct Person 
+struct Person1
 {
     name: String,
     age: u8,
 }
 
+// Tuple struct
+struct Person2(String, u8);
+
 pub fn run() {
-    let mut p1 = Person {
+    let mut p1 = Person1 {
         name: String::from("Rohan"),
         age: 26,
     };
     println!("p1 = person named {} aged {}", p1.name, p1.age);
     p1.age += 1;
     println!("p1 = person named {} aged {}", p1.name, p1.age);
+
+    let mut p2 = Person2 (
+        String::from("Brohan"),
+        26,
+    );
+    println!("p2 = person named {} aged {}", p2.0, p2.1);
+    p2.1 += 1;
+    println!("p2 = person named {} aged {}", p2.0, p2.1);
 }

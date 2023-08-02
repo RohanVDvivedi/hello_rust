ed6ad33debe830b365d862d8f7bd413419cee4f4
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:23:33 -0700

    printing bianry hex and octal  umbers

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,5 @@
 pub fn run() {
     println!("Hello {0} !!, {0}, likes to {1}", "Rohan", "code");
     println!("Hello {name} !!, {name}, likes to {activity}", name = "Rohan", activity = "code");
+    println!("Binary {:b}, Hexadecimal {:x}, Octal {:o}", 77, 77, 77);
 }

e4b86323420edd0b92fc657c61c4b26c20b1a2af
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 08:27:05 -0700

    defined my first struct in rust

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,15 @@
-pub fn run() {
 
+// Traditional c style struct
+struct Person 
+{
+    name: String,
+    age: u8,
+}
+
+pub fn run() {
+    let p1 = Person {
+        name: String::from("Rohan"),
+        age: 26,
+    };
+    println!("p1 = person named {} aged {}", p1.name, p1.age);
 }

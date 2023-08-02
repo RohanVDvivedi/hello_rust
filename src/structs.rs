2d9c7f57f1a0fbaeba721827d9d03db0b917fabe
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 08:46:06 -0700

    implemented a new function for Tuple structures struct

--- src/structs.rs
+++ src/structs.rs
@@ -18,16 +18,22 @@ impl Person1 {
 // Tuple struct
 struct Person2(String, u8);
 
+impl Person2 {
+    fn new(name : &str, age : u8) -> Person2 {
+        return Person2 (
+            String::from(name),
+            age,
+        );
+    }
+}
+
 pub fn run() {
     let mut p1 = Person1::new("Rohan", 26);
     println!("p1 = person named {} aged {}", p1.name, p1.age);
     p1.age += 1;
     println!("p1 = person named {} aged {}", p1.name, p1.age);
 
-    let mut p2 = Person2 (
-        String::from("Brohan"),
-        26,
-    );
+    let mut p2 = Person2::new("Brohan", 26);
     println!("p2 = person named {} aged {}", p2.0, p2.1);
     p2.1 += 1;
     println!("p2 = person named {} aged {}", p2.0, p2.1);

36aa832bd0f4eb17094233b6f703dce7dff38b2d
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 09:57:54 -0700

    built a simple enum match switch case

---
+++ src/enums.rs
@@ -0,0 +1,24 @@
+enum Movement {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn move_av(m : Movement) {
+    match m {
+        Movement::Up => println!("Up"),
+        Movement::Down => println!("Down"),
+        Movement::Left => println!("Left"),
+        Movement::Right => println!("Right"),
+    }
+}
+
+pub fn run() {
+    move_av(Movement::Up);
+    move_av(Movement::Up);
+    move_av(Movement::Down);
+    move_av(Movement::Left);
+    move_av(Movement::Left);
+    move_av(Movement::Right);
+}

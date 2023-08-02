89b59078ea094cf8264b3cac97aa6c542f178004
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:29:39 -0700

    printing solutions to expression

--- src/print.rs
+++ src/print.rs
@@ -2,4 +2,5 @@ pub fn run() {
     println!("Hello {0} !!, {0}, likes to {1}", "Rohan", "code");
     println!("Hello {name} !!, {name}, likes to {activity}", name = "Rohan", activity = "code");
     println!("Binary {0:b}, Hexadecimal {0:x}, Decimal {0}, Octal {0:o}", 77);
+    println!("77 + 7 * 7 = {}", 77 + 7 * 7);
 }

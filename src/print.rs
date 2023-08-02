cf725f5c425e413fb528c2d224c9f1d0467d0ac3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:27:06 -0700

    using positional arguments with traits

--- src/print.rs
+++ src/print.rs
@@ -1,5 +1,5 @@
 pub fn run() {
     println!("Hello {0} !!, {0}, likes to {1}", "Rohan", "code");
     println!("Hello {name} !!, {name}, likes to {activity}", name = "Rohan", activity = "code");
-    println!("Binary {:b}, Hexadecimal {:x}, Decimal {}, Octal {:o}", 77, 77, 77, 77);
+    println!("Binary {0:b}, Hexadecimal {0:x}, Decimal {0}, Octal {0:o}", 77);
 }

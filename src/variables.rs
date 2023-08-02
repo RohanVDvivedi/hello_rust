e06348ecd6d908671ef03a5d4826c874bf8a9ac5
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:44:11 -0700

    mutable and immutable variables

--- src/variables.rs
+++ src/variables.rs
@@ -3,5 +3,9 @@
 // rust is block scoped language
 
 pub fn run() {
-
+    let name = "Rohan";
+    let mut age = 26;
+    println!("My name is {}, aged {}", name, age);
+    age += 1;
+    println!("My name is {}, aged {}", name, age);
 }

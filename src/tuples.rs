b3625b76c109659bcabd26f237d0fb0718e85540
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 05:12:59 -0700

    tuples source to build and print details

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,9 @@
+// tuples group together values, with max 12 elements
+
 pub fn run() {
+    let t : (&str, &str, i8) = ("Rohan", "Gujarat", 26);
+
+    println!("{:?}", t);
+    println!("{} is from {}, aged {}", t.0, t.1, t.2);
 
 }

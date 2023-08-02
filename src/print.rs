146ccc6f5a07d9cd0919b8f906c7df7f2069d218
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:20:40 -0700

    named parameters with println

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,4 @@
 pub fn run() {
     println!("Hello {0} !!, {0}, likes to {1}", "Rohan", "code");
+    println!("Hello {name} !!, {name}, likes to {activity}", name = "Rohan", activity = "code");
 }

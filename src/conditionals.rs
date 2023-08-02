001406733a03d3e6ec60bfab610c17cfb74eceb2
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 05:51:07 -0700

    --

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,9 +1,12 @@
 pub fn run() {
-    let age = 16;
+    let age = 60;
+    let id = true;
     if age < 20 {
         println!("No drinks for you");
-    } else if age < 30 {
+    } else if age < 40 {
         println!("You must be at work");
+    } else if (age < 100) && !id {
+        println!("Go get your ID");
     } else {
         println!("Enjoy you retired fellow !!");
     }

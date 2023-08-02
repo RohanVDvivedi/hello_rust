e041e96d5aae990eaa55dc0b9773e099cee0d950
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 06:26:53 -0700

    implemented a simple add function git status YAY

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,14 @@
 pub fn run () {
     greeting("Hello", "Rohan");
+    let a : i32 = 32;
+    let b : i32 = 64;
+    println!("{} + {} = {}", a, b, add(a,b));
 }
 
 fn greeting(greet : &str, name : &str) {
     println!("{} !! , {}", greet, name);
+}
+
+fn add(n1 : i32, n2 : i32) -> i32 {
+    return n1 + n2;
 }

222d173c5b0b047c30753593458338e934ee6707
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 01:53:55 -0700

    added necessary comments and default types for variables

--- src/variables.rs
+++ src/variables.rs
@@ -4,15 +4,18 @@ pub fn run() {
     // variables are immutable by default
     // rust is block scoped language
     let name = "Rohan";
-    let mut age = 26;
-    println!("My name is {}, aged {}", name, age);
+    let mut age = 26; // default to i32
+    let weight = 73.5; // default to f64
+    println!("My name is {}, aged {}, weighs {}", name, age, weight);
     age += 1;
-    println!("My name is {}, aged {}", name, age);
+    println!("My name is {}, aged {}, weighs {}", name, age, weight);
 
 
     // primitive types of rust are 
     // i8 u8 to i128 u128 -> i -> int and u -> unsigned int, with explicity number of bits
     // f32, f64
     // bool, char
-    // tuples and arrays
+    // tuples and arrays -> discussed later
+
+    // rust is statically typed langauge i.e. all types known at compile time, but there is string strict type inference
 }

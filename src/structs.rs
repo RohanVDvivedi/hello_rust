f2e6bcbf39c54e2e638e993a80855a9554c05f88
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-02 08:55:38 -0700

    added method to mutate the structs, &mut self is a must

--- src/structs.rs
+++ src/structs.rs
@@ -16,6 +16,9 @@ impl Person1 {
     fn describe(&self) -> String {
         return format!("{} is aged {}.", self.name, self.age);
     }
+    fn age_by_an_year(&mut self) {
+        self.age += 1;
+    }
 }
 
 // Tuple struct
@@ -31,16 +34,19 @@ impl Person2 {
     fn describe(&self) -> String {
         return format!("{} is aged {}.", self.0, self.1);
     }
+    fn age_by_an_year(&mut self) {
+        self.1 += 1;
+    }
 }
 
 pub fn run() {
     let mut p1 = Person1::new("Rohan", 26);
     println!("p1 = {}", p1.describe());
-    p1.age += 1;
+    p1.age_by_an_year();
     println!("p1 = {}", p1.describe());
 
     let mut p2 = Person2::new("Brohan", 26);
     println!("p2 = {}", p2.describe());
-    p2.1 += 1;
+    p2.age_by_an_year();
     println!("p2 = {}", p2.describe());
 }

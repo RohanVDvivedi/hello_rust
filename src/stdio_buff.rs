bffec8a8ebba72bf7737acc9ac3efd4ad9ea0c6c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-03 00:24:13 -0700

    formatted source

--- src/stdio_buff.rs
+++ src/stdio_buff.rs
@@ -1,9 +1,10 @@
-
 pub fn run() {
     println!("Enter your age");
-    let mut age : String = String::new();
-    std::io::stdin().read_line(&mut age).expect("No input received");
-    let mut age : u8 = age.trim().parse().expect("Age provided was not a number");
-    age+=1;
+    let mut age: String = String::new();
+    std::io::stdin()
+        .read_line(&mut age)
+        .expect("No input received");
+    let mut age: u8 = age.trim().parse().expect("Age provided was not a number");
+    age += 1;
     println!("Your age anext year will be {}", age);
 }
